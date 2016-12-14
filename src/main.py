abe8153e57dd41003fca939f2b21045d615e5352
hh <hh@example.com>
2016-12-14 22:13:31 +0000

    Sends them all out in json format

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
+import sys
+import json
 import socket
+
 import dns.resolver
 import dns.query
 import dns.zone
 
 ## Initial add of hostname
 
-query_domain = 'google.co.uk'
+query_domain = sys.argv[1]
 
 # Get the nameservers of the domain
 
@@ -14,7 +17,7 @@ def fetch_records(query_domain, record_type) :
 
     try :
         for record in dns.resolver.query(query_domain, record_type) :
-            record_array.append(record)
+            record_array.append(str(record))
 
         return record_array
 
@@ -26,15 +29,9 @@ result = {}
 
 for record in record_list:
     result[record] = fetch_records(query_domain, record)
-    print record + " >> "
-    print result[record]
 
-# Try and do a zone transfer
-#z = dns.zone.from_xfr(dns.query.xfr('204.152.189.147', 'dnspython.org'))
-#names = z.nodes.keys()
-#names.sort()
-#for n in names:
-#    print z[n].to_text(n)
+print json.dumps(result, indent=4)
+#json.dumps(result)
 
 # if a zone transfer is not supported, do lookups on the most common subdomains
 
