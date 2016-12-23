76acfe2e27c20a654bde95598a1d23e92f507346
hh <hh@example.com>
2016-12-23 16:59:02 +0000

    wrap save in function

--- src/main.py
+++ src/main.py
@@ -8,6 +8,8 @@ import dns.resolver
 import dns.query
 import dns.zone
 
+from flask import Flask, url_for, json, request
+
 query_domain = sys.argv[1]
 log_root_directory = sys.argv[2]
 
@@ -23,6 +25,23 @@ def fetch_records(query_domain, record_type) :
     except dns.resolver.NoAnswer:
         return ['no records']
 
+# Create domain directory if it does not already exist and save with timestamp
+def save(result):
+    # Wrap up in json
+    result_json = json.dumps(result, indent=4)
+    log_file_timestamp = datetime.datetime.now().strftime("_%A_%d_%B_%Y_%I_%M%p")
+    log_file_name = query_domain + log_file_timestamp + '.json'
+    log_file_full_path = log_root_directory + '/' + query_domain + '/' + log_file_name
+
+    log_directory = log_root_directory + '/' + query_domain
+
+    if not os.path.exists(log_directory):
+        os.makedirs(log_directory)
+
+    log_file = open(log_file_full_path, "w")
+    log_file.write(result_json)
+    log_file.close()
+
 
 # Lookups on the most common subdomains
 record_list = ['NS', 'MX', 'A', 'TXT', 'CNAME', 'AAAA']
@@ -31,23 +50,7 @@ result = {}
 for record in record_list:
     result[record] = fetch_records(query_domain, record)
 
-# Wrap up in json file
-result_json = json.dumps(result, indent=4)
-
-# Create domain directory if it does not already exist and save with timestamp
-log_file_timestamp = datetime.datetime.now().strftime("_%A_%d_%B_%Y_%I_%M%p")
-log_file_name = query_domain + log_file_timestamp + '.json'
-log_file_full_path = log_root_directory + '/' + query_domain + '/' + log_file_name
-
-log_directory = log_root_directory + '/' + query_domain
-
-if not os.path.exists(log_directory):
-    os.makedirs(log_directory)
-
-log_file = open(log_file_full_path, "w")
-log_file.write(result_json)
-log_file.close()
-
+save(result)
 
 ## Scheduled check
 
