793557663dd6fec4bdc25f2a91d1d409e060ea14
hh <hh@example.com>
2017-01-01 15:55:40 +0000

    Compares latest file to see if anything has changed and only then, writes a new log

--- src/storage.py
+++ src/storage.py
@@ -9,7 +9,7 @@ class Storage:
 
     def __init__(self, hostname):
         self.hostname = hostname
-        self.log_directory = 'logs'
+        self.log_directory = 'logs/' + hostname
         self.limit = 10
 
         # set flags on which storage system to use
@@ -19,47 +19,70 @@ class Storage:
         return self.log_directory + '/' + self.hostname
 
     def list_files(self):
-        directory = self.get_log_directory()
-        files = sorted(os.listdir(directory))
+        files = sorted(os.listdir(self.log_directory))
 
         file_list = files[0:self.limit]
 
         return file_list
 
     def get_records(self):
-        directory = self.get_log_directory()
         files = self.list_files()
 
         result = {}
         i = 0
 
         for file_name in reversed(files):
-            json_string = file_get_contents(directory + '/' + file_name)
+            json_string = file_get_contents(self.log_directory + '/' + file_name)
             result[file_name] = json.loads(json_string)
             i = i + 1
 
         return result
 
+    def get_newest_filename(self):
+        files = self.list_files()
+
+        if files:
+            return files[-1]
+        else:
+            return False
+
     # TODO
     def dns_changed(self, result):
-        stored_records = self.get_records()
-        return False
+        newest_filename = self.get_newest_filename()
+
+        # DNS has changes as we have no record if it
+        if newest_filename is False:
+            return False
+
+        newest_json_string = file_get_contents(self.log_directory + '/' + newest_filename)
+
+        # Set both to json so a comparison can be made
+        sent = result
+        sent_json =  json.dumps(sent)
+
+        newest = json.loads(newest_json_string)
+        newest_json = json.dumps(newest)
+
+        if newest_json == sent_json :
+            return False
+        else:
+            return True
 
     def save_flat(self, result):
+
+        if not os.path.exists(self.log_directory):
+            os.makedirs(self.log_directory)
 
         result_json = json.dumps(result, indent=4)
 
         # If exactly the same, nothing to do
-        if self.dns_changed(result) is not False:
+        if self.dns_changed(result) is False:
             return True
 
         timestamp_format = "_%y-%m-%dT%H-%M-%S"
         log_file_timestamp = datetime.datetime.now().strftime(timestamp_format)
         log_file_name = self.hostname + log_file_timestamp + '.json'
-        log_file_full_path = self.log_directory + '/' + self.hostname + '/' + log_file_name
-
-        if not os.path.exists(self.log_directory):
-            os.makedirs(self.log_directory)
+        log_file_full_path = self.log_directory + '/' + log_file_name
 
         try:
             log_file = open(log_file_full_path, "w")
@@ -80,7 +103,6 @@ class Storage:
             return self.save_flat(result)
 
     def load(self):
-
         if self.flat_system:
             return self.load_flat()
 
